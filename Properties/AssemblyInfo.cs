fa3134ec9cade9b707e2b396d3a1f70d4b125e05
Prof. Miguel Angel Medina-Pérez, PhD <47543798+miguelmedinaperez@users.noreply.github.com>
2020-09-08 09:44:28 -0500

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("BaggingRandomMinerApp")]
+[assembly: AssemblyTitle("eUD35")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BaggingRandomMinerApp")]
+[assembly: AssemblyProduct("eUD35")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
